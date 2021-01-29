d51284b8cff973387edf5a30cb3e67e485e419d4
Jaysurya9 <46379345+Jaysurya9@users.noreply.github.com>
2021-01-29 20:40:22 +0530

    Create about.php

---
+++ Wordpress Projects/Restaurant Page/about.php
@@ -0,0 +1,95 @@
+<!-- wp:cover {"url":"https://b.zmtcdn.com/data/pictures/6/2801276/5774ba051dfeea503484b2c782fc00e9.jpg?output-format=webp\u0026amp;fit=around|1029:555\u0026amp;crop=1029:555;*,*","id":217,"hasParallax":true,"dimRatio":20,"customOverlayColor":"#000000","align":"full","className":"is-style-default"} -->
+<div class="wp-block-cover alignfull has-background-dim-20 has-background-dim has-parallax is-style-default" style="background-image:url(https://b.zmtcdn.com/data/pictures/6/2801276/5774ba051dfeea503484b2c782fc00e9.jpg?output-format=webp&amp;fit=around|1029:555&amp;crop=1029:555;*,*);background-color:#000000"><div class="wp-block-cover__inner-container"><!-- wp:separator -->
+<hr class="wp-block-separator"/>
+<!-- /wp:separator -->
+
+<!-- wp:spacer {"height":20} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"textAlign":"center"} -->
+<h2 class="has-text-align-center">Enjoy Our Food Experience</h2>
+<!-- /wp:heading -->
+
+<!-- wp:spacer {"height":20} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:separator -->
+<hr class="wp-block-separator"/>
+<!-- /wp:separator --></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:spacer {"height":84} -->
+<div style="height:84px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"textAlign":"center","level":4} -->
+<h4 class="has-text-align-center">Welcome to New District 12</h4>
+<!-- /wp:heading -->
+
+<!-- wp:spacer {"height":20} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph -->
+<p>An array of mouthwatering food from cuisines all over the world can be found at New District 12 Restaurant, Vizag.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>While the delicate and soft interiors relax you, our delicious food tantalizes your taste buds. Savor the multitude of dishes provided at our top Vizag restaurant.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":84} -->
+<div style="height:84px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:cover {"url":"https://rockfielddemo.files.wordpress.com/2019/08/blur-chef-close-up-671956-1.jpg","id":218,"hasParallax":true,"dimRatio":20,"customOverlayColor":"#000000","align":"full","className":"is-style-bottom-wave"} -->
+<div class="wp-block-cover alignfull has-background-dim-20 has-background-dim has-parallax is-style-bottom-wave" style="background-image:url(https://rockfielddemo.files.wordpress.com/2019/08/blur-chef-close-up-671956-1.jpg);background-color:#000000"><div class="wp-block-cover__inner-container"><!-- wp:separator -->
+<hr class="wp-block-separator"/>
+<!-- /wp:separator -->
+
+<!-- wp:spacer {"height":20} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"textAlign":"center"} -->
+<h2 class="has-text-align-center">Discover Our Menu</h2>
+<!-- /wp:heading -->
+
+<!-- wp:spacer {"height":20} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:separator -->
+<hr class="wp-block-separator"/>
+<!-- /wp:separator --></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:spacer {"height":84} -->
+<div style="height:84px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph -->
+<p>The menu is a mix of French&nbsp;regional cuisines, and the menu changes with the seasons.<br>Be sure to ask about the daily specials!</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":32} -->
+<div style="height:32px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:separator -->
+<hr class="wp-block-separator"/>
+<!-- /wp:separator -->
+
+<!-- wp:spacer {"height":32} -->
+<div style="height:32px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
